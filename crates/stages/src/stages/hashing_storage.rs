bd86a6745fc0fd2637956fd89b28cef7e3c4d078
rakita <dragan0rakita@gmail.com>
2023-01-09 11:43:44 +0100

    Unwind StorageHashingStage

--- crates/stages/src/stages/hashing_storage.rs
+++ crates/stages/src/stages/hashing_storage.rs
@@ -2,7 +2,7 @@ use crate::{
     db::Transaction, ExecInput, ExecOutput, Stage, StageError, StageId, UnwindInput, UnwindOutput,
 };
 use reth_db::{
-    cursor::{DbCursorRO, DbCursorRW, DbDupCursorRO},
+    cursor::{DbCursorRO, DbCursorRW, DbDupCursorRO, DbDupCursorRW},
     database::Database,
     models::TransitionIdAddress,
     tables,
@@ -73,9 +73,9 @@ impl<DB: Database> Stage<DB> for StorageHashingStage {
 
                 if let Some((next_key, _)) = next_key {
                     first_key = next_key;
-                    continue
+                    continue;
                 }
-                break
+                break;
             }
         } else {
             // read storage changeset, merge it into one changeset and calculate storage hashes.
@@ -152,10 +152,52 @@ impl<DB: Database> Stage<DB> for StorageHashingStage {
     /// Unwind the stage.
     async fn unwind(
         &mut self,
-        _tx: &mut Transaction<'_, DB>,
+        tx: &mut Transaction<'_, DB>,
         input: UnwindInput,
     ) -> Result<UnwindOutput, StageError> {
-        // TODO read StorageChangeSet, set old values and add/delete removed/created storage slots.
+        let from_transition_rev =
+            if input.unwind_to == 0 { 0 } else { tx.get_block_transition(input.unwind_to - 1)? };
+        let to_transition_rev = tx.get_block_transition(input.stage_progress)?;
+
+        let mut hashed_storage = tx.cursor_dup_mut::<tables::HashedStorage>()?;
+
+        // Aggregate all transition changesets and and make list of account that have been changed.
+        tx.cursor::<tables::StorageChangeSet>()?
+            .walk((from_transition_rev, H160::zero()).into())?
+            .take_while(|res| {
+                res.as_ref()
+                    .map(|(TransitionIdAddress((k, _)), _)| *k <= to_transition_rev)
+                    .unwrap_or_default()
+            })
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .rev()
+            // fold all account to get the old balance/nonces and account that needs to be removed
+            .fold(
+                BTreeMap::new(),
+                |mut accounts: BTreeMap<(Address, H256), U256>,
+                 (TransitionIdAddress((_, address)), storage_entry)| {
+                    accounts.insert((address, storage_entry.key), storage_entry.value);
+                    accounts
+                },
+            )
+            .into_iter()
+            // hash addresses and collect it inside sorted BTreeMap.
+            // We are doing keccak only once per address.
+            .map(|((address, key), value)| ((keccak256(address), keccak256(key)), value))
+            .collect::<BTreeMap<_, _>>()
+            .into_iter()
+            // Apply values to HashedStorage (if Value is zero remove it);
+            .try_for_each(|((address, key), value)| -> Result<(), StageError> {
+                hashed_storage.seek_by_key_subkey(address, key)?;
+                hashed_storage.delete_current()?;
+
+                if value != U256::ZERO {
+                    let new_entry = StorageEntry { key, value };
+                    hashed_storage.append_dup(address, new_entry)?;
+                }
+                Ok(())
+            })?;
 
         Ok(UnwindOutput { stage_progress: input.unwind_to })
     }
